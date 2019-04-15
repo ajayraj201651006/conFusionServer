f3c5a5fbf204a0048f95efe835ed34cae7d1712f
ajayraj201651006 <ajayraj003016@gmail.com>
2019-04-15 14:02:52 +0530

    create logout session using express-session

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-    name: {
+    username: {
         type: String,
         required: true,
         unique: true
--- routes/users.js
+++ routes/users.js
@@ -10,27 +10,27 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('./signup', (req, res, next) => {
+router.post('/signup', (req, res, next) => {
   User.findOne({username: req.body.username})
     .then((user) => {
        if(user != null) {
-         var err = new Error('User ' + req.body.username + 'already exists!');
+         var err = new Error('User ' + req.body.username + ' already exists!');
          err.status = 403;
          next(err);
        } else {
          return User.create({
               username: req.body.username,
               password: req.body.password
-        })
+        });
       }
     })
     .then((user) => {
             res.statusCode = 200;
-            res.setHeader = ('Content-Type', 'application/json');
-            res.json({status: 'Registration successfull !', user: user});
+            res.setHeader('Content-Type', 'application/json');
+            res.json({status: 'Registration successfull!', user: user});
           }, (err) => next(err))
           .catch((err) => next(err));
-    });
+});
 
 router.post('/login', (req, res, next) => {
 
@@ -71,9 +71,22 @@ router.post('/login', (req, res, next) => {
        .catch((err) => next(err));
   }
     else {
-      req.statusCode 200;
+      req.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
       res.end('You are already authenticated!');
+    }
+});
+
+router.get('/logout', (req, res) => {
+   if(req.session) {
+     req.session.destroy();
+     res.clearCookie('session-id');
+     res.redirect('/');
+   }
+    else {
+      var err = new Error('You have not logged in!');
+      err.status = 403;
+      next(err);
     }
 });
 
