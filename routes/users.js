43e4fc3035e1cd7c820acfa062e7afb546deefd9
ajayraj201651006 <ajayraj003016@gmail.com>
2019-04-17 14:08:11 +0530

    signup using passport

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 const bodyPraser = require('body-parser');
 var User = require('../models/user');
 
+var passport = require('passport');
+
 var router = express.Router();
 router.use(bodyPraser.json());
 
@@ -11,25 +13,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup', (req, res, next) => {
-  User.findOne({username: req.body.username})
-    .then((user) => {
-       if(user != null) {
-         var err = new Error('User ' + req.body.username + ' already exists!');
-         err.status = 403;
-         next(err);
-       } else {
-         return User.create({
-              username: req.body.username,
-              password: req.body.password
+  User.register(new User({username: req.body.username}),
+      req.body.password, (err, user) => {
+       if(err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({err: err});
+       }
+        else {
+          passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration successfull!'});
         });
       }
-    })
-    .then((user) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({status: 'Registration successfull!', user: user});
-          }, (err) => next(err))
-          .catch((err) => next(err));
+    });
 });
 
 router.post('/login', (req, res, next) => {
