9554ecbc72bab8e7886e605d550f00113531b30c
ajayraj201651006 <ajayraj003016@gmail.com>
2019-05-17 12:30:24 +0530

    OAuth2 login using facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/confusion'
+    'mongoUrl': 'mongodb://localhost:27017/confusion',
+    'facebook': {
+        clientId: '403844193534243',
+        clientSecret: 'f4c02027bb291e8f8c90ae3be0d720be'
+    }
 }
